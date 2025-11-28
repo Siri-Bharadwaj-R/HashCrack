d2d9b65f45d4e9eee5a4f6095fd57f0da934493a
Siri <151047839+Siri-Bharadwaj-R@users.noreply.github.com>
2025-11-28 21:46:24 +0530

    Add common passwords to builtin_wordlist.py
    Added a list of common passwords for use in the application.

---
+++ cracker /builtin_wordlist.py
@@ -0,0 +1,98 @@
+COMMON_WORDS = [
+
+    # --- Ultra-common passwords ---
+    "password", "password1", "password123", "password1234", "password2024", "password2025",
+    "pass", "pass123", "pass@123", "passw0rd", "p@ssword", "p@ssw0rd",
+    "admin", "admin1", "admin123", "admin@123", "administrator", "admin007",
+    "root", "root123", "root@123", "root007", "toor",
+
+    # --- Numeric sequences ---
+    "123", "1234", "12345", "123456", "1234567", "12345678", "123456789",
+    "111111", "000000", "999999",
+    "1212", "112233", "123123",
+
+    # --- Keyboard patterns ---
+    "qwerty", "qwerty123", "qwertyuiop",
+    "asdf", "asdf123", "asdfgh",
+    "zxcvbn", "zxcvbn123",
+
+    # --- Very common global passwords ---
+    "letmein", "welcome", "welcome123",
+    "login", "user", "username", "guest", "guest123",
+    "default", "default123",
+    "test", "test123",
+    "demo", "demo123",
+
+    # --- Popular names ---
+    "john", "john123",
+    "david", "david123",
+    "michael", "michael123",
+    "rahul", "rahul123",
+    "arjun", "arjun123",
+    "sai", "sai123",
+    "krishna123", "shiva123", "ganesh123",
+
+    # --- Indian context passwords ---
+    "india", "india123", "india@123", "india2024", "india2025",
+    "bharat", "bharat123", "bharat2024",
+    "modi", "modi123",
+    "omkar", "omkar123",
+    "delhi123", "mumbai123", "bangalore123",
+
+    # --- Year-based patterns ---
+    "2020", "2021", "2022", "2023", "2024", "2025",
+    "newyear2024", "newyear2025",
+
+    # --- Tech/Hacker passwords ---
+    "hacker", "hacker123",
+    "cyber", "cyber123", "cyber@123",
+    "matrix", "matrix123",
+    "shadow", "shadow123",
+    "dragon", "dragon123",
+    "pokemon", "pokemon123",
+    "naruto", "naruto123",
+    "goku", "goku123",
+
+    # --- Social media inspired ---
+    "insta123", "instagram", "instagram123",
+    "facebook123", "fb123",
+    "snapchat123",
+
+
+    # --- Sports / teams ---
+    "football", "football123",
+    "cricket", "cricket123",
+    "viratkohli", "viratkohli18",
+    "ronaldo7", "messi10",
+
+    # --- Pet/common words ---
+    "tiger", "tiger123",
+    "lion", "lion123",
+    "kitty123",
+    "puppy123",
+
+    # --- Easy substitutions ---
+    "admin2024", "admin2025",
+    "system123",
+    "secure123",  # ironically common
+
+    # --- Birthday-like patterns ---
+    "010203", "020202", "101010",
+    "15082001", "14022002", "01012000",
+
+    # --- Extra weak passwords ---
+    "abc123", "abcd1234", "abc@123",
+    "mypass", "mypass123", "mypassword",
+    "superman", "superman123",
+
+    # --- Default router/modem passwords ---
+    "adminadmin", "useruser",
+    "passwordadmin",
+
+    # --- Device-related ---
+    "wifi123", "wifi@123",
+    "mobile123", "phone123",
+
+    # --- Special characters variants ---
+    "admin!23", "admin#123", "pass#123", "pass!23",
+]
